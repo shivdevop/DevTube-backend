6b00cca3d3b183ffcb4255956daf5cf15068074e
shivam <shivamagarwalla05@gmail.com>
2025-08-15 21:03:00 +0530

    added Playlist model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+      }
+    ],
+    isPublic: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
